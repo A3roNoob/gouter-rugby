db6bb5119d71121de248897cb88cab64899448fd
Debrej <t.bellanger34@gmail.com>
2017-11-21 11:56:43 +0100

    - fix de l'affichage de la page d'accueil - nouvelles images pour la page d'accueil - rajout des 's' a la fin de 'gouter' dans le titre -ajout d'une fonction pour recuperer un parametre dans un url dans util.js

--- util.js
+++ util.js
@@ -91,4 +91,17 @@ function checkRang1(rang){
     }
 }
 
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+          tmp = item.split("=");
+          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
+}
+
 document.head.innerHTML += "\n<link rel=\"icon\" href=\"images/rugby-ball.ico\" />\n";
