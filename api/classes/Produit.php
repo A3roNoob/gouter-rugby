956f71c731600113c363268ed9830214e82f6eea
EmeraldWhovian <boutryguillaume1@gmail.com>
2018-01-08 18:20:59 +0100

    API ENDPOINT /api/produit/ajouter, /api/produit/supprimer, /produit/modifier
    /api/produit/ajouter
    
    PARAM POST: token, login, nom, desc, prix
    
    RETOUR ATTENDU: CODE 0
    
    /api/produit/supprimer
    
    PARAM POST: token, login, idproduit
    
    RETOUR ATTENDU: CODE 0
    
    /api/produit/modifier
    
    PARAM POST: token, login, idproduit et (nom ou desc ou prix)
    
    RETOUR ATTENDU: CODE 0

--- api/classes/Produit.php
+++ api/classes/Produit.php
@@ -92,6 +92,29 @@ class Produit implements JsonSerializable{
         return $prod;
     }
 
+    public static function creerProduit($nom, $desc, $prix){
+        $prod = new self();
+        $prod->setNom($nom);
+        $prod->setDescProduit($desc);
+        $prod->setPrix($prix);
+        return $prod;
+    }
+
+    public function enregistrerProduit(){
+        $db = DatabaseObject::connect();
+        $query = $db->prepare("INSERT INTO produit(nomProduit, descProduit, prix) VALUES(:nom, :descr, :prix)");
+        $query->bindValue(':nom', $this->getNom());
+        $query->bindValue(':descr', $this->getDescProduit());
+        $query->bindValue(':prix', $this->getPrix());
+
+        try{
+            $query->execute();
+        }catch(PDOException $e){
+            echo '{"Code" : ' . $GLOBALS['CODE']['CODE_5']['Code'] . ', "Message" : "' . $GLOBALS['CODE']['CODE_5']['Message'] . '", "INFOS" : "' . $e->getMessage() . '"}';
+            exit(1);
+        }
+    }
+
     public function hydrate(array $data){
             if(isset($data['idProduit']))
                 $this->setIdProduit($data['idProduit']);
@@ -103,6 +126,41 @@ class Produit implements JsonSerializable{
                 $this->setPrix($data['prix']);
             if(isset($data['quantite']))
                 $this->setQuantite($data['quantite']);
+    }
+
+    public static function loadProduit($idProduit){
+        $db = DatabaseObject::connect();
+        $query = $db->prepare("SELECT * FROM produit WHERE idProduit=:id");
+        $query->bindValue(':id', $idProduit, PDO::PARAM_INT);
+        try{
+            $query->execute();
+            $data = $query->fetch(PDO::FETCH_ASSOC);
+        }catch(PDOException $e){
+            echo '{"Code" : ' . $GLOBALS['CODE']['CODE_5']['Code'] . ', "Message" : "' . $GLOBALS['CODE']['CODE_5']['Message'] . '", "INFOS" : "' . $e->getMessage() . '"}';
+            exit(1);
+        }
+
+        if(!is_bool($data)){
+            $produit = new self();
+            $produit->hydrate($data);
+            return $produit;
+        }
+        return false;
+    }
+
+    public function updateProduit(){
+        $db = DatabaseObject::connect();
+        $query = $db->prepare("UPDATE produit SET nomProduit=:nom, descProduit=:descr, prix=:prix WHERE idProduit=:id");
+        $query->bindValue(':id', $this->getIdProduit(), PDO::PARAM_INT);
+        $query->bindValue(':nom', $this->getNom());
+        $query->bindValue(':descr', $this->getDescProduit());
+        $query->bindValue(':prix', $this->getPrix());
+        try{
+            $query->execute();
+        }catch(PDOException $e){
+            echo '{"Code" : ' . $GLOBALS['CODE']['CODE_5']['Code'] . ', "Message" : "' . $GLOBALS['CODE']['CODE_5']['Message'] . '", "INFOS" : "' . $e->getMessage() . '"}';
+            exit(1);
+        }
     }
 
     public function verifierProduit(){
@@ -120,6 +178,23 @@ class Produit implements JsonSerializable{
         if(!$data)
             return false;
         return true;
+    }
+
+    public function supprimerProduit(){
+        if($this->verifierProduit()){
+            $db = DatabaseObject::connect();
+            $query = $db->prepare("DELETE FROM produit WHERE idProduit=:id");
+            $query->bindValue(':id', $this->getIdProduit(), PDO::PARAM_INT);
+            try{
+                $query->execute();
+            }catch(PDOException $e){
+                echo '{"Code" : ' . $GLOBALS['CODE']['CODE_5']['Code'] . ', "Message" : "' . $GLOBALS['CODE']['CODE_5']['Message'] . '", "INFOS" : "' . $e->getMessage() . '"}';
+                exit(1);
+            }
+        }else{
+            echo '{"Code" : ' . $GLOBALS['CODE']['CODE_18']['Code'] . ', "Message" : "' . $GLOBALS['CODE']['CODE_18']['Message'] . '", "IDS" : [' . $this->getIdProduit() . ']}';
+            exit(1);
+        }
     }
 
     public function jsonSerialize()
--- api/produit.php
+++ api/produit.php
@@ -29,4 +29,86 @@ if (isGetSet('action') && $_GET['action'] == "produits") {
     } else {
         echo '{"Code" : ' . $GLOBALS['CODE']['CODE_1']['Code'] . ', "Message" : "' . $GLOBALS['CODE']['CODE_1']['Message'] . '"}';
     }
+} else if(isGetSet('action') && $_GET['action'] == 'supprimer'){
+    if (isPostSet('token') && isPostSet('login')) {
+        $token = test_input($_POST['token']);
+        $login = test_input($_POST['login']);
+        $adulte = Adulte::loadByLogin($login);
+        if ($adulte->checkToken($token)) {
+            if (isPostSet('idproduit')) {
+                if ($adulte->getIdRang() < 3) {
+                    $produit = new Produit();
+                    $produit->setIdProduit(test_input($_POST['idproduit']));
+                    $produit->supprimerProduit();
+                    echo '{"Code" : ' . $GLOBALS['CODE']['CODE_0']['Code'] . ', "Message" : "' . $GLOBALS['CODE']['CODE_0']['Message'] . '"}';
+                } else {
+                    echo '{"Code" : ' . $GLOBALS['CODE']['CODE_403']['Code'] . ', "Message" : "' . $GLOBALS['CODE']['CODE_403']['Message'] . '"}';
+                }
+            } else {
+                echo '{"Code" : ' . $GLOBALS['CODE']['CODE_1']['Code'] . ', "Message" : "' . $GLOBALS['CODE']['CODE_1']['Message'] . '"}';
+            }
+        } else {
+            echo '{"Code" : ' . $GLOBALS['CODE']['CODE_8']['Code'] . ', "Message" : "' . $GLOBALS['CODE']['CODE_8']['Message'] . '"}';
+        }
+    } else {
+        echo '{"Code" : ' . $GLOBALS['CODE']['CODE_1']['Code'] . ', "Message" : "' . $GLOBALS['CODE']['CODE_1']['Message'] . '"}';
+    }
+}else if(isGetSet('action') && $_GET['action'] == 'ajouter'){
+    if (isPostSet('token') && isPostSet('login')) {
+        $token = test_input($_POST['token']);
+        $login = test_input($_POST['login']);
+        $adulte = Adulte::loadByLogin($login);
+        if ($adulte->checkToken($token)) {
+            if (isPostSet('nom') && isPostSet("desc") && isPostSet("prix")) {
+                if ($adulte->getIdRang() < 3) {
+                    $produit = Produit::creerProduit(test_input($_POST["nom"]), test_input($_POST['desc']), test_input($_POST['prix']));
+                    $produit->enregistrerProduit();
+                    echo '{"Code" : ' . $GLOBALS['CODE']['CODE_0']['Code'] . ', "Message" : "' . $GLOBALS['CODE']['CODE_0']['Message'] . '"}';
+                } else {
+                    echo '{"Code" : ' . $GLOBALS['CODE']['CODE_403']['Code'] . ', "Message" : "' . $GLOBALS['CODE']['CODE_403']['Message'] . '"}';
+                }
+            } else {
+                echo '{"Code" : ' . $GLOBALS['CODE']['CODE_1']['Code'] . ', "Message" : "' . $GLOBALS['CODE']['CODE_1']['Message'] . '"}';
+            }
+        } else {
+            echo '{"Code" : ' . $GLOBALS['CODE']['CODE_8']['Code'] . ', "Message" : "' . $GLOBALS['CODE']['CODE_8']['Message'] . '"}';
+        }
+    } else {
+        echo '{"Code" : ' . $GLOBALS['CODE']['CODE_1']['Code'] . ', "Message" : "' . $GLOBALS['CODE']['CODE_1']['Message'] . '"}';
+    }
+}else if(isGetSet('action') && $_GET['action'] == 'modifier'){
+    if (isPostSet('token') && isPostSet('login')) {
+        $token = test_input($_POST['token']);
+        $login = test_input($_POST['login']);
+        $adulte = Adulte::loadByLogin($login);
+        if ($adulte->checkToken($token)) {
+            if (isPostSet('idproduit') && (isPostSet('nom') || isPostSet("desc") || isPostSet("prix"))) {
+                if ($adulte->getIdRang() < 3) {
+                    $produit = Produit::loadProduit(test_input($_POST['idproduit']));
+                    if(is_bool($produit)){
+                        echo '{"Code" : ' . $GLOBALS['CODE']['CODE_18']['Code'] . ', "Message" : "' . $GLOBALS['CODE']['CODE_18']['Message'] . '", "IDS" : [' . $_POST['idproduit'] . ']}';
+                        exit(1);
+                    }
+                    if(isPostSet('nom'))
+                        $produit->setNom(test_input($_POST['nom']));
+                    if(isPostSet('desc'))
+                        $produit->setDescProduit(test_input($_POST['desc']));
+                    if(isPostSet('prix'))
+                        $produit->setPrix(test_input($_POST['prix']));
+                    $produit->updateProduit();
+                    echo '{"Code" : ' . $GLOBALS['CODE']['CODE_0']['Code'] . ', "Message" : "' . $GLOBALS['CODE']['CODE_0']['Message'] . '"}';
+                } else {
+                    echo '{"Code" : ' . $GLOBALS['CODE']['CODE_403']['Code'] . ', "Message" : "' . $GLOBALS['CODE']['CODE_403']['Message'] . '"}';
+                }
+            } else {
+                echo '{"Code" : ' . $GLOBALS['CODE']['CODE_1']['Code'] . ', "Message" : "' . $GLOBALS['CODE']['CODE_1']['Message'] . '"}';
+            }
+        } else {
+            echo '{"Code" : ' . $GLOBALS['CODE']['CODE_8']['Code'] . ', "Message" : "' . $GLOBALS['CODE']['CODE_8']['Message'] . '"}';
+        }
+    } else {
+        echo '{"Code" : ' . $GLOBALS['CODE']['CODE_1']['Code'] . ', "Message" : "' . $GLOBALS['CODE']['CODE_1']['Message'] . '"}';
+    }
+}else {
+    header('Location: /api/');
 }
